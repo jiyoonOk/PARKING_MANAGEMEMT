afd05419d8e6ffca32f2fb80f046f89e320e1796
bwallha <bwallha910@gmail.com>
2022-12-05 16:42:31 +0900

    menu

---
+++ src/Menu.java
@@ -0,0 +1,42 @@
+package menu;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class Menu extends JFrame{
+	public Menu () {
+		JLabel mTitle = new JLabel("MENU");
+		mTitle.setFont(mTitle.getFont().deriveFont(45.0f));
+		
+		JButton mMypage = new JButton("마이페이지");
+		JButton mNotice = new JButton("공지사항");
+		JButton mQuest = new JButton("문의사항");
+		JButton mAbout = new JButton("ABOUT");
+		JButton mLogout = new JButton("LOGOUT");
+		
+		//위치, 크기 설정
+		mTitle.setBounds(130, 10, 200, 100);
+		mMypage.setBounds(150, 180, 100, 30);
+		mNotice.setBounds(150, 230, 100, 30);
+		mQuest.setBounds(150, 280, 100, 30);
+		mAbout.setBounds(150, 330, 100, 30);
+		mLogout.setBounds(250, 430, 100, 30);
+		
+		Container menuCt = getContentPane();
+		menuCt.setLayout(null);
+		menuCt.add(mTitle);
+		menuCt.add(mMypage);
+		menuCt.add(mNotice);
+		menuCt.add(mQuest);
+		menuCt.add(mAbout);
+		menuCt.add(mLogout);
+	}
+	public static void main(String[] args) {
+		Menu menu = new Menu();
+		menu.setTitle("MENU");
+		menu.setSize(400, 600);
+		menu.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		menu.setVisible(true);
+	}
+
+}
